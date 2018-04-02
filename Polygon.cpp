178c36c40d9314410ac82bf76d4c25f6fc81f76e
Brandon Abbott <barabbit907@gmail.com>
2018-04-02 13:36:02 -0800

    polygon done

--- Polygon.cpp
+++ Polygon.cpp
@@ -11,7 +11,7 @@ using std::ostream;
 using std::endl;
 using std::ostringstream;
 
-Polygon::Polygon(double numSides, double sideLength)
+Polygon::Polygon(double numSides, double sideLength) //change sideLength to size
 :_numSides(numSides), _sideLength(sideLength)
 {}
 
@@ -34,11 +34,38 @@ double Polygon::getSideLength() const{
 }
 
 ostringstream & Polygon::toPostScript(ostringstream & stream){
-    stream << "/numSides {" << getNumSides() << "} def" << endl;
-    stream << "/sideLength {" << getSideLength() << "} def" << endl;
-    stream << "newpath" << endl;
-    stream << "72 72 moveto" << endl;
-    stream << "" << endl;
+
+    stream << "/defpoly {" << endl;
+    stream << "4 dict begin /p exch def /n exch def" << endl;
+    stream << "/m matrix currentmatrix def" << endl;
+    stream << " dup scale" << endl;
+    stream << "/s matrix currentmatrix def" << endl;
+    stream << "0 360 n div 359.9"
+    stream << "({" << endl;
+    stream << "{" << endl;
+    stream << "//s setmatrix" << endl;
+    stream << "rotate" << endl;
+    stream << "//p exec" << endl;
+    stream << "} for" << endl;
+    stream << "//m setmatrix" << endl;
+    stream << "}) token pop exch pop" << endl;
+    stream << "bind" << endl;
+    stream << "end exec" << endl;
+    stream << "} bind def" << endl;
+    stream << endl;
+    stream << "/drawpoly {" << endl;
+    stream << "gsave" << endl;
+    stream << "translate" << endl;
+    stream << "1 index 0 moveto" << endl;
+    stream << "{" << endl;
+    stream << "1 0 lineto" << endl;
+    stream << "} defpoly" << endl;
+    stream << "closepath stroke" << endl;
+    stream << "grestore" << endl;
+    stream << "} def" << endl;
+    stream << "100" << getNumSides(); << getHeight(); << getWidth(); << "drawpoly"<< endl;
+    //this is the how its set up I just gave it a set size of 100 but used
+    // your functions to complete the rest.  
+    //100 6 300 200 drawpoly %size, #sides, lenght?, width?
     return stream;
 }
-
